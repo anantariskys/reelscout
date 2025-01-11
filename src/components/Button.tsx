795fdeb7cfd80a44b170134212c33a34390ba92a
anantariskys <gamersindo555@gmail.com>
2025-01-11 10:52:23 +0700

    feat(button) : add new variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,29 +1,45 @@
-import  { FC, ReactNode } from 'react'
+import { FC, ReactNode } from "react";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
-    variant?: 'primary' | 'secondary',
-    children : ReactNode,
-    width? : 'w-fit' | 'w-full'
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?:
+    | "primary"
+    | "secondary"
+    | "primary-outline"
+    | "secondary-outline"
+    | "tertiary";
+  children: ReactNode;
+  width?: "w-fit" | "w-full";
 }
 
-const Button:FC<ButtonProps> = ({children,variant,width='w-fit'}) => {
-    let className = ""
-    switch (variant) {
-        case 'primary':
-            className = "bg-primary text-white"
-            break;
-        case 'secondary':
-            className = "bg-secondary text-white"
-            break;
-        default:
-            className = "bg-primary text-white"
-            break;
-    }
+const Button: FC<ButtonProps> = ({ children, variant, width = "w-fit" }) => {
+  let className = "";
+  switch (variant) {
+    case "primary":
+      className = "bg-primary text-white";
+      break;
+    case "primary-outline":
+      className =
+        "bg-transparent border border-primary text-primary hover:bg-primary hover:text-white";
+      break;
+    case "secondary":
+      className = "bg-secondary text-white";
+      break;
+    case "secondary-outline":
+      className =
+        "bg-transparent border border-secondary text-secondary hover:bg-secondary hover:text-white";
+      break;
+    case "tertiary":
+      className = "bg-white text-primary";
+      break;
+    default:
+      className = "bg-primary text-white";
+      break;
+  }
   return (
     <button className={`${className} ${width} h-fit rounded-xl py-2 px-8`}>
       {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
