44f8563e307587eb661ab51b7419112376f6cee3
anantariskys <gamersindo555@gmail.com>
2025-01-11 11:00:33 +0700

    feat : add few types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,1 +1,80 @@
-declare module '@splidejs/react-splide'
+export interface Movie {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+  }
+
+  type Genre = {
+    id: number;
+    name: string;
+  };
+  
+  type ProductionCompany = {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  };
+  
+  type ProductionCountry = {
+    iso_3166_1: string;
+    name: string;
+  };
+  
+  type SpokenLanguage = {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  };
+  
+  export interface Video {
+    iso_639_1: string;
+    iso_3166_1: string;
+    name: string;
+    key: string;
+    site: string;
+    size: number; 
+    type: string;
+    official: boolean; 
+    published_at: string;
+    id: string;
+  }
+  export type MovieDetail = {
+    adult: boolean;
+    backdrop_path: string | null;
+    belongs_to_collection: string | null;
+    budget: number;
+    genres: Genre[];
+    homepage: string | null;
+    id: number;
+    imdb_id: string | null;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string | null;
+    production_companies: ProductionCompany[];
+    production_countries: ProductionCountry[];
+    release_date: string;
+    revenue: number;
+    runtime: number;
+    spoken_languages: SpokenLanguage[];
+    status: string;
+    tagline: string | null;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+  };
+  
